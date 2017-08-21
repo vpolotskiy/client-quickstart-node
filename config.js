7a9f92675e83ea1653d9c1c3c99282160cbf69ec
Vitaliy Polotskiy <vpolotskiy@gmail.com>
2017-08-21 19:11:51 +0300

    Removed dotenv from production

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-const dotenv = require('dotenv');
-const cfg = {};
-
-if (process.env.NODE_ENV !== 'test') {
-  dotenv.config({path: '.env'});
-} else {
-  dotenv.config({path: '.env.example', silent: true});
+if (process.env.NODE_ENV === 'development') {
+  const dotenv = require('dotenv');
+  dotenv.config();
 }
+
+const cfg = {};
 
 // HTTP Port to run our web application
 cfg.port = process.env.PORT || 3000;
